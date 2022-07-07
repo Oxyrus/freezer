5588baa6d4be67ff38553d7594f4fe7d36dbf4e7
Andrés Pérez <im.oxyrus@gmail.com>
2022-07-06 21:50:27 -0500

    Initial commit

---
+++ src/main/java/com/oxyrus/freezer/Freezer.java
@@ -0,0 +1,37 @@
+package com.oxyrus.freezer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Freezer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        ItemStack iceBlocks = new ItemStack(Material.ICE, 64);
+
+        ItemMeta meta = iceBlocks.getItemMeta();
+        meta.setDisplayName("§bIce Block");
+
+        iceBlocks.setItemMeta(meta);
+
+        NamespacedKey key = new NamespacedKey(this, "ice_block");
+
+        ShapedRecipe recipe = new ShapedRecipe(key, iceBlocks);
+
+        recipe.shape(" W ", " S ", "   ");
+        recipe.setIngredient('W', Material.WATER_BUCKET);
+        recipe.setIngredient('S', Material.SNOW_BLOCK);
+
+        Bukkit.addRecipe(recipe);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
